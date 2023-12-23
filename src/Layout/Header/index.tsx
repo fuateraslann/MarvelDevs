9b80145b4731e415c7bbb8b9a4a2c29d302a1135
fuateraslann <fuatze@gmail.com>
2023-12-23 14:53:24 +0300

    header text color changed

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -1,6 +1,7 @@
-import { AppBar, Toolbar, Grid } from '@mui/material'
+import { AppBar, Toolbar, Grid, useTheme } from '@mui/material'
 import Logo from 'assets/Marvel_Logo.png'
 const Header = () => {
+  const theme = useTheme()
   return (
     <AppBar position="static" sx={{ backgroundColor: 'white' }}>
       <Toolbar>
@@ -13,7 +14,7 @@ const Header = () => {
             />
           </Grid>
           <Grid item xs={5} sm={5} sx={{ textAlign: 'center', color: 'black' }}>
-            <h1>Marvel Devs </h1>
+            <h1 style={{ color: theme.palette.primary.main }}>Marvel Devs </h1>
           </Grid>
           <Grid item xs={3} sm={3}></Grid>
         </Grid>
